9ef4a977962ef27ebfbf0503ca5e2cea9a0e53d5
HasnainSakib <hasnainsakibnfs@gmail.com>
2023-10-26 15:29:43 +0600

    I did it for practice purpose

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -37,14 +37,4 @@ $home=Home::find($id);
 
 }
 
-
-// <div class="mb-3" style="background-color: rgb(0, 0, 0)">
-//     <label for="formGroupExampleInput"  class="form-label">First Name</label>
-//     <input type="text" name="F_name" class="form-control" id="formGroupExampleInput" placeholder="Enter your First Name">
-//   </div>
-//   <div class="mb-3" style="background-color: rgb(12, 10, 10)">
-//     <label for="formGroupExampleInput2" class="form-label">Last Name</label>
-//     <input type="text" name="L_name" class="form-control" id="formGroupExampleInput2" placeholder="Enter your Last Name">
-//   </div>
-
 }
